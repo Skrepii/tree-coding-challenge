192595952ce39dd905e7d041f9e309dddd297d13
Matiss Cikota <matisscikota@gmail.com>
2019-11-23 15:22:57 +0100

    added better error handling, logic, comments and transactions

--- app/app.ts
+++ app/app.ts
@@ -1,26 +1,98 @@
-import express = require('express');
+import express, { Request, Response, Application } from 'express';
 import { Node, swapParent } from './model/node';
 import mongoose = require('mongoose');
 
-const app: express.Application = express();
-const PORT: number = 8080;
+const app: Application = express();
+const PORT = process.env.PORT || 8080;
+const mongoURL = process.env.MONGOURL || 'mongodb://mongo:27017/mongo';
 
-app.get('/', async (_, res) => {
-    res.json(await Node.find({}));
+// Parses incoming request bodies as JSON
+app.use(express.json());
+
+// Gives all available Nodes from the database
+app.get('/', async (_, res: Response) => {
+    try {
+        res.json(await Node.find({}));
+    } catch (err) {
+        res.status(500).json({
+            error: 'Nodes not found!',
+            message: 'Make sure that the database is running'
+        });
+    }
 });
 
-app.get('/:id', async (req, res) => {
-    res.json(await Node.findById(req.params['id']).populate('children'));
+/* 
+    Searches for a node with a query id
+    
+    Example search:
+    localhost:8080/node?id=5dd83a9fc308c0001f893d83
+
+    - If found returns an JSON object with Node properties
+    - If a node with a given id is not found 'null' is returned
+    - If an id format is incorrect then error message is returned
+*/
+app.get('/node', async (req: Request, res: Response) => {
+    const { id } = req.query;
+
+    if (!id) {
+        res.status(400).json({
+            error: 'Missing parameters!',
+            message: 'Make sure that key \'id\' is specified'
+        });
+        return;
+    }
+
+    try {
+        res.json(await Node.findById(id).populate('children'));
+    } catch (err) {
+        res.status(400).json({
+            error: 'Failed to find!',
+            message: 'Incorrect id format'
+        });
+    }
 });
 
-app.get('/:id1/toparent/:id2', async (req, res) => {
-    res.json(await swapParent(req.params['id1'], req.params['id2']));
-})
+/* 
+    Swaps parentNode for a node
+    Requires POST JSON body with parameters childId and parentId
+    
+    Example search:
+    localhost:8080/node
+    {
+        'childId': '5dd8381726fbce001e0ce3b7',
+	    'parentId': '5dd8381726fbce001e0ce3b5'
+    }
+
+    - If swap was successful then mainNodeId, parentNode, oldParentNodeId (if exists) 
+        will be returned as JSON object
+    - If one of the nodes will not be found or incorrect id format will be given 
+        during a swap an error message will be returned
+*/
+app.post('/node', async (req: Request, res: Response) => {
+    const { childId, parentId } = req.body;
+    if (!childId) {
+        res.status(400).json({
+            error: 'Missing parameters!',
+            message: 'Make sure that keys \'childId\' and \'parentId\' are specified'
+        });
+        return;
+    }
+
+    try {
+        res.json(await swapParent(childId, parentId));
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
 app.listen(PORT, async () => {
-    await mongoose.connect('mongodb://mongo:27017/mongo', { useNewUrlParser: true, useUnifiedTopology: true });
-    if (process.env.POPULATE_DB === 'true') {
-        await populateDatabase();
+    try {
+        await mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true });
+        if (process.env.POPULATE_DB === 'true') {
+            await populateDatabase();
+        }
+    } catch (err) {
+        console.error('Failed to connect to mongoDB server\n', err);
     }
     console.log('Listening on port:', PORT)
 });
--- app/model/node.ts
+++ app/model/node.ts
@@ -1,4 +1,4 @@
-import { Document, Schema, model, Model } from "mongoose";
+import { Document, Schema, model, Model, ClientSession } from "mongoose";
 
 interface INode extends Document {
     description: string;
@@ -22,19 +22,69 @@ const NodeSchema: Schema = new Schema({
 });
 
 NodeSchema.methods.setParent = function (node: INode | null): Promise<INode | null> {
-    return new Promise(async (resolve) => {
+    return new Promise(async (resolve, reject) => {
+
+        let session: ClientSession = await Node.db.startSession();
+        session.startTransaction();
+
+        let previousRoot: INode | null = null;
         let parentNode: INode | null = null;
-        if (node) {
-            if (this.parentNode) {
-                parentNode = await Node.findById(this.parentNode);
+
+        if (this.parentNode) {
+            try {
+                parentNode = await Node.findById(this.parentNode);   
                 if (parentNode) {
                     parentNode.removeChild(this._id);
                 }
+            } catch (err) {
+                console.warn('Failed to find parent node\n', err);
+            }        
+        }
+
+        // Check if the node we're changing to is null, if so then we set it as root
+        // and add previous root as a child
+        if (!node) {
+            try {
+                // Finding the previous root node
+                previousRoot = await Node.findById(this.rootNode);
+                if (previousRoot) {
+                    previousRoot.parentNode = this._id;
+                    this.children.push(previousRoot._id);
+                }
+            } catch (err) {
+                console.warn('Failed to find previous root node\n', err);
             }
+
+            this.parentNode = null;
+            this.height = 0;
+            this.rootNode = this._id;
+        } else {
             this.parentNode = node._id;
+            this.height = node.height + 1;
             node.children.push(this._id);
         }
-        resolve(parentNode);
+
+        try {
+            await this.save();
+            if (node) {
+                await node.save();
+            }
+            if (parentNode) {
+                await parentNode.save();
+            }
+            if (previousRoot) {
+                await previousRoot.save();
+            }
+            (this.rootNode === this._id) ? await this.calibrateHeightRoot(this._id) : await this.calibrateHeightRoot();
+            await session.commitTransaction();
+            session.endSession();
+            resolve(parentNode);
+        } catch (err) {
+            console.error('Error saving files\n', err);
+            await session.abortTransaction(); // Rollback
+            session.endSession();
+            reject(err);
+        }
     });
 }
 
@@ -69,29 +119,32 @@ NodeSchema.methods.calibrateHeightRoot = function (root?: string): Promise<void>
 
 export const Node: Model<INode> = model<INode>("Node", NodeSchema);
 
-export let swapParent = async function (mainNodeID: string, parentNodeID: string): Promise<object> {
-    return new Promise(async (resolve) => {
-        let message: any = {
-            description: 'Failed to swap'
-        };
-        let main: INode | null = await Node.findById(mainNodeID);
-        let parent: INode | null = await Node.findById(parentNodeID);
-        if (main && parent) {
-            let oldParent: INode | null = await main.setParent(parent);
-            if (oldParent) {
-                oldParent.save();
-                message.oldParentNode = oldParent._id;
+export let swapParent = async function (childNodeId: string, parentNodeID: string | null): Promise<object> {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let main: INode | null = await Node.findById(childNodeId);
+            if (main) {
+                let result: any = {};
+
+                let parent: INode | null = (parentNodeID) ? await Node.findById(parentNodeID) : null;
+                let oldParent: INode | null = await main.setParent(parent);
+                
+                if (oldParent) {
+                    result.oldParentNode = oldParent._id;
+                }
+                result.mainNode = main._id;
+                result.parentNode = (parent) ? parent._id : null;
+                resolve(result);
             }
-            main.height = parent.height + 1;
-            await main.save();
-            await parent.save();
-
-            await main.calibrateHeightRoot();
-
-            message.description = 'Swap successful'
-            message.mainNode = main._id;
-            message.parentNode = parent._id;
+            reject({
+                error: 'Node not found!',
+                message: 'childNodeId can\'t be found'
+            });
+        } catch (err) {
+            reject({
+                error: 'Failed to swap!',
+                message: 'Child or parent node does not exist'
+            });
         }
-        resolve(message);
     });
 }
